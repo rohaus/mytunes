60b44540a52af0d39008a392b0bb69a3484f1b7d
Hack Reactor Students <ryan@rohaus.us>
2013-10-09 22:01:53 -0700

    Completes SongQueueView Specs

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,19 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.collection.on('add', function(){this.render();}, this);
+    this.collection.on('remove', function(){this.render();}, this);
   },
 
   render: function() {
-    return this.$el;
+    return this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model:song}).render();
+      })
+    );
   }
 
 });
